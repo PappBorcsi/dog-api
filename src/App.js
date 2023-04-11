9d0513c525991b7b8a924485f1637b11bdb77d54
PappBorcsi <92024772+PappBorcsi@users.noreply.github.com>
2023-04-11 13:07:28 +0200

    breed and subBreed selection

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ export default function App() {
       .then((b) => b.json())
       .then((b) => {
         const { message: breedsResponse } = b;
-
         const breedsArray = Object.entries(breedsResponse);
-
         setBreeds(breedsArray);
       })
       .catch((e) => {
@@ -19,30 +17,59 @@ export default function App() {
       });
   }, []);
 
-  // console.log(breeds);
-
-  const subBreed = (sB) => {
-    return sB.map((b, index) => b + (index === sB.length - 1 ? '' : ', '));
-  };
-
   return (
     <div>
       <h1>Hello!</h1>
       <p>Here are the dog breeds :)</p>
-      <ul>
-        {breeds &&
-          breeds.map((breed) => {
-            /* 
-            Sub breeds:
-            console.log(breed[1])
-            */
-            return (
-              <li>
-                {breed[0]}s{subBreed(breed[1])}
-              </li>
-            );
-          })}
-      </ul>
+      <BreedsSelect breedList={breeds} />
     </div>
   );
 }
+
+//componens lh
+const BreedsSelect = ({ breedList }) => {
+  const [breedSelect, setBreedSelect] = useState();
+  const [subBreedSelect, setsubBreedSelect] = useState();
+  const [subBreedList, setsubBreedList] = useState();
+
+  const handleBreedChange = (event) => {
+    const actualBreed = event.target.value;
+    setBreedSelect(actualBreed);
+    const actualSubBreeds = getSelectedSubBreeds(actualBreed);
+    setsubBreedList(actualSubBreeds);
+  };
+
+  const handleSubBreedChange = (event) => {
+    setsubBreedSelect(event.target.value);
+  };
+
+  const getSelectedSubBreeds = (breed) => {
+    const subBreeds = breedList.find((br) => br[0] == breed);
+    if (subBreeds) {
+      return subBreeds[1];
+    }
+    return [];
+  };
+
+  // console.log('Sub Breed', getSelectedSubBreeds(breedSelect));
+
+  return (
+    <>
+      <select value={breedSelect} onChange={handleBreedChange}>
+        <option value="">-</option>
+        {breedList.map((breed) => {
+          return <option value={breed[0]}>{breed[0]}</option>;
+        })}
+      </select>
+
+      {subBreedList && subBreedList.length > 0 && (
+        <select value={subBreedSelect} onChange={handleSubBreedChange}>
+          <option value="">-</option>
+          {subBreedList.map((sbName) => (
+            <option value={sbName}>{sbName}</option>
+          ))}
+        </select>
+      )}
+    </>
+  );
+};
